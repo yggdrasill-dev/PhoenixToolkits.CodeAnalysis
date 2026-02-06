5d49368604f6a13f0125f5ff8b7e2f1173dbfb15
Phoenix <fphoenix923@hotmail.com>
2026-02-07 02:01:06 +0800

    feat: 升級 Roslyn 套件並移除 VB 測試工具
    - 升級 Roslyn、MSTest、VSSDK 套件至新版本
    - 移除 Visual Basic 測試驗證工具及相關檔案
    - C# 測試工具改用 DefaultVerifier
    - 專案目標框架升級至 net48
    - vsix 安裝目標改為 VS 17.0+ 並更新描述

--- PhoenixToolkits.CodeAnalysis/PhoenixToolkits.CodeAnalysis.Test/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
+++ PhoenixToolkits.CodeAnalysis/PhoenixToolkits.CodeAnalysis.Test/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
@@ -1,25 +1,25 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.Testing;
 
 namespace PhoenixToolkits.CodeAnalysis.Test;
 
 public static partial class CSharpAnalyzerVerifier<TAnalyzer>
-	where TAnalyzer : DiagnosticAnalyzer, new()
+    where TAnalyzer : DiagnosticAnalyzer, new()
 {
-	public class Test : CSharpAnalyzerTest<TAnalyzer, MSTestVerifier>
-	{
-		public Test()
-		{
-			SolutionTransforms.Add((solution, projectId) =>
-			{
-				var compilationOptions = solution.GetProject(projectId).CompilationOptions;
-				compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
-					compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
-				solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+    public class Test : CSharpAnalyzerTest<TAnalyzer, DefaultVerifier>
+    {
+        public Test()
+        {
+            SolutionTransforms.Add((solution, projectId) =>
+            {
+                var compilationOptions = solution.GetProject(projectId).CompilationOptions;
+                compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
+                    compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+                solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
 
-				return solution;
-			});
-		}
-	}
+                return solution;
+            });
+        }
+    }
 }
--- PhoenixToolkits.CodeAnalysis/PhoenixToolkits.CodeAnalysis.Test/Verifiers/CSharpCodeFixVerifier`2+Test.cs
+++ PhoenixToolkits.CodeAnalysis/PhoenixToolkits.CodeAnalysis.Test/Verifiers/CSharpCodeFixVerifier`2+Test.cs
@@ -2,51 +2,51 @@
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.Testing;
 
 namespace PhoenixToolkits.CodeAnalysis.Test;
 
 public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
-	where TAnalyzer : DiagnosticAnalyzer, new()
-	where TCodeFix : CodeFixProvider, new()
+    where TAnalyzer : DiagnosticAnalyzer, new()
+    where TCodeFix : CodeFixProvider, new()
 {
-	public class Test : CSharpCodeFixTest<TAnalyzer, TCodeFix, MSTestVerifier>
-	{
-		public Test()
-		{
-			SolutionTransforms.Add((solution, projectId) =>
-			{
-				var compilationOptions = solution.GetProject(projectId).CompilationOptions;
-				compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
-					compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
-				solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+    public class Test : CSharpCodeFixTest<TAnalyzer, TCodeFix, DefaultVerifier>
+    {
+        public Test()
+        {
+            SolutionTransforms.Add((solution, projectId) =>
+            {
+                var compilationOptions = solution.GetProject(projectId).CompilationOptions;
+                compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
+                    compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+                solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
 
-				return solution;
-			});
-		}
-	}
+                return solution;
+            });
+        }
+    }
 
-	public class CS4033Test : Test
-	{
-		public CS4033Test()
-		{
-			SolutionTransforms.Add((solution, projectId) =>
-			{
-				var compilationOptions = solution.GetProject(projectId).CompilationOptions;
-				compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
-					compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
-				solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+    public class CS4033Test : Test
+    {
+        public CS4033Test()
+        {
+            SolutionTransforms.Add((solution, projectId) =>
+            {
+                var compilationOptions = solution.GetProject(projectId).CompilationOptions;
+                compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
+                    compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+                solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
 
-				return solution;
-			});
-		}
+                return solution;
+            });
+        }
 
-		protected override DiagnosticDescriptor GetDefaultDiagnostic(DiagnosticAnalyzer[] analyzers) => new(
-			"CS4033",
-			string.Empty,
-			string.Empty,
-			string.Empty,
-			DiagnosticSeverity.Error,
-			false);
-	}
+        protected override DiagnosticDescriptor GetDefaultDiagnostic(DiagnosticAnalyzer[] analyzers) => new(
+            "CS4033",
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            DiagnosticSeverity.Error,
+            false);
+    }
 }
